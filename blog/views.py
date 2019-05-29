47c17c01fa498436ec9a4469a95eef49b35e8d78
penny3947 <penny3947@hanmail.net>
2019-05-30 07:43:57 +0900

    weird... company version

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import InputForm
+
 
 # Create your views here.
 def post_list(request):
+    if request.method == "POST":
+        #print(request.years)
+        print("POST")
+        form = InputForm(request.POST)
+        if form.is_valid():
+            year = form.cleaned_data["years"]
+            month = form.cleaned_data["months"]
+            print(year,",",month)
+    else:
+        form = InputForm()
+
     return render(request, 'blog/post_list.html', {})
