df5011b376af18314ac555c81823d1c85a831a1a
penny3947 <penny3947@hanmail.net>
2019-06-01 15:48:13 +0900

    Changed parameter from 2 to 3

--- blog/getChgRate.py
+++ blog/getChgRate.py
@@ -9,15 +9,15 @@ def is_leap(target_year):     # 윤년 판단
         return 28
 
 
-def tillWhen(target_period): # 해당 월의 마지막 날짜 판단 (몇일자까지 api호출해야 하는지 판단)
-    till31 = ["01", "03", "05", "07", "08", "10", "12"]
-    till30 = ["04", "06", "09", "11"]
+def till_when(target_period): # 해당 월의 마지막 날짜 판단 (몇일자까지 api호출해야 하는지 판단)
+    till31 = ["01", "03", "05", "07", "08", "10", "12"] # 31일까지 있는 달
+    till30 = ["04", "06", "09", "11"]                   # 30일까지 있는 달
 
     if target_period == datetime.datetime.now().strftime("%Y%m"):    # 당월의 경우, 현재일까지만
         return datetime.datetime.now().day
-    elif target_period[4:6] in till31:   # 31일까지 있는 달
+    elif target_period[4:6] in till31:
         return 31
-    elif target_period[4:6] in till30:   # 30일까지 있는 달
+    elif target_period[4:6] in till30:
         return 30
     else:                               # 2월 (윤년 판단 추가)
         return is_leap(int(target_period[0:4]))
@@ -33,7 +33,7 @@ def getChgRate(in_val):
 
     target_period = in_val["year"] + in_val["month"]
     curr = in_val["curr"]
-    end_date = tillWhen(target_period)
+    end_date = till_when(target_period)
 
     for day in range(1, end_date+1):
         target_date = target_period + '{:0>2}'.format(day)
